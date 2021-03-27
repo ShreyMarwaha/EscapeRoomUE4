69c7883f615664c2c97117e4860773b20f2df2ae
ShreyMarwaha <shrey19334@iiitd.ac.in>
2021-03-28 02:11:30 +0530

    Refactored Code (mainly UGrabber::TickComponent() in Grabber.cpp)

--- EscapeRoom/Source/EscapeRoom/Grabber.cpp
+++ EscapeRoom/Source/EscapeRoom/Grabber.cpp
@@ -4,67 +4,96 @@
 #include "Grabber.h"
 #include <Runtime/Engine/Public/DrawDebugHelpers.h>
 #define OUT
+
 // Sets default values for this component's properties
-UGrabber::UGrabber()
-{
+UGrabber::UGrabber(){
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
 // Called when the game starts
-void UGrabber::BeginPlay()
-{
+void UGrabber::BeginPlay(){
 	Super::BeginPlay();
 
-	// ...
-	UE_LOG(LogTemp, Warning, TEXT("Grabber Reporting for duty"));
+	FindPhysicsHandleComponent();
+	SetupInputComponent();
+}
+
+
+void UGrabber::FindPhysicsHandleComponent() {
+	/// Look for attached Physics handle
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	if (PhysicsHandle)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Physics Handle component Found"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s missing Physics Handle component"), *GetOwner()->GetName());
+	}
+}
+
+void UGrabber::SetupInputComponent() {
+	/// Look for attached InputComponent (only appears at the runtime)
+	InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+	if (PhysicsHandle)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Input Component found"));
+		InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+		InputComponent->BindAction("Grab", IE_Released, this, &UGrabber::Release);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s Missing Input component"), *GetOwner()->GetName());
+	}
+}
+
+
+
+void UGrabber::Grab(){
+	UE_LOG(LogTemp, Warning, TEXT("Grab Pressed"));
+
+	/// LINE TRACE and see if we reach any actors with the physics body collision chaned set
+	GetFirstPhysicsBodyInReach();
+
+	/// iF we hit something attach a pysics handle
+	//TODO: attach physics handle
+}
+
+void UGrabber::Release(){
+	UE_LOG(LogTemp, Warning, TEXT("Grab released"));
+	// TODO: release physics handle
 }
 
 
 // Called every frame
-void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction){
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+	// if the physics hadle is attached
+		// move the obj the we are holding
+}
 
+
+FHitResult UGrabber::GetFirstPhysicsBodyInReach() const {
 	/// Get player view point this tick
 	FVector PlayerViewPointLocation;
 	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerViewPointLocation, OUT PlayerViewPointRotation);
 
-	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint( OUT PlayerViewPointLocation, OUT PlayerViewPointRotation);
-	
-	//UE_LOG(LogTemp, Warning, TEXT(" vec: %s rot: %s"), *PlayerViewPointLocation.ToString(), *PlayerViewPointRotation.ToString());
-
-	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector()*Reach;
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
 
 	///Draw a red trace in theh world to visualise
-	DrawDebugLine(
-		GetWorld(),
-		PlayerViewPointLocation,
-		LineTraceEnd,
-		FColor(255, 0, 0),
-		false,
-		0.f,
-		0.f,
-		10.f
-	);
+	DrawDebugLine(GetWorld(), PlayerViewPointLocation, LineTraceEnd, FColor(255, 0, 0), false, 0.f, 0.f, 10.f);
 
 	///setup query params
 	FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
 
 	///Line trace (AKA ray-cast) out to reach distance
 	FHitResult Hit;
-	GetWorld()->LineTraceSingleByObjectType(
-		OUT Hit,
-		PlayerViewPointLocation,
-		LineTraceEnd,
-		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
-		TraceParameters
-
-	);
+	GetWorld()->LineTraceSingleByObjectType(OUT Hit, PlayerViewPointLocation, LineTraceEnd, FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody), TraceParameters);
 
 	///See what we hit
 	AActor* ActorHit = Hit.GetActor();
@@ -72,5 +101,6 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	{
 		UE_LOG(LogTemp, Warning, TEXT("Line Trace hit: %s"), *(ActorHit->GetName()));
 	}
-}
 
+	return FHitResult();
+}
--- EscapeRoom/Source/EscapeRoom/Grabber.h
+++ EscapeRoom/Source/EscapeRoom/Grabber.h
@@ -4,28 +4,46 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include <Runtime/Engine/Classes/PhysicsEngine/PhysicsHandleComponent.h>
+#include "Components/InputComponent.h"
 #include "Grabber.generated.h"
+
 
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class ESCAPEROOM_API UGrabber : public UActorComponent
 {
-	GENERATED_BODY()
+	GENERATED_BODY() //ignore if error
 
 public:	
 	// Sets default values for this component's properties
 	UGrabber();
 
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
 private:
-	//How far the hea dof the player cab we reach in cm
+	//How far the head of the player cab we reach in cm
 	float Reach = 100.f;
-		
+	UPhysicsHandleComponent* PhysicsHandle = nullptr;
+	UInputComponent* InputComponent = nullptr;
+
+	// Ray cast and grab what is in reach
+	void Grab();
+
+	//Called when grab is Released
+	void Release();
+
+	//Find (assumed) attached physics component
+	void FindPhysicsHandleComponent();
+
+	//Setup (assumed) attached input component
+	void SetupInputComponent();
+
+	// Return hit for first physics body in reach
+	FHitResult GetFirstPhysicsBodyInReach() const;
 };
